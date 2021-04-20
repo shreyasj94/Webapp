05757bcbf5826ab9bd18b4dcb9ecfec5679564d8
shreyasvj94 <shreyasvjcfc@gmail.com>
2021-04-20 16:42:19 -0500

    commit

---
+++ src/main/java/com/shreyas/webapp/services/IngredientService.java
@@ -0,0 +1,13 @@
+package com.shreyas.webapp.services;
+
+import com.shreyas.webapp.commands.IngredientCommand;
+
+
+public interface IngredientService {
+
+    IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId);
+
+    IngredientCommand saveIngredientCommand(IngredientCommand command);
+
+    void deleteById(Long recipeId, Long idToDelete);
+}
